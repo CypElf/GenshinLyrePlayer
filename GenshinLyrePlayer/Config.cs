ff8bd6c2d62dbe6690c1290996b07385a8955b05
CypElf <cypelf.pro@gmail.com>
2021-12-18 16:29:42 +0100

    added customizable start and stop keys

--- GenshinLyrePlayer/Config.cs
+++ GenshinLyrePlayer/Config.cs
@@ -5,5 +5,7 @@
         public string keyboardLayout { get; set; }
         public bool useAutoRoot { get; set; }
         public int? customRoot { get; set; }
+        public string startKey { get; set; }
+        public string stopKey { get; set; }
     }
 }
--- GenshinLyrePlayer/MainWindow.xaml.cs
+++ GenshinLyrePlayer/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace GenshinLyrePlayer
         private List<FileStream> midiFiles = new List<FileStream>();
         private Playback playback;
         private bool useAutoRoot;
+        private string startKeyBackup;
+        private string stopKeyBackup;
+        private bool listeningForStartKey = false;
+        private bool listeningForStopKey = false;
 
         private bool configLoaded = false;
 
@@ -77,37 +81,88 @@ namespace GenshinLyrePlayer
 
         private void onKeyPressed(object sender, KeyPressedArgs e)
         {
-            Debug.WriteLine("PRESSED " + e.KeyPressed.ToString());
-
-            if (e.KeyPressed == Key.F7 && playback != null && playback.IsRunning)
+            if (!listeningForStartKey && !listeningForStopKey)
             {
-                playback.Stop();
-                playingTextBlock.Text = "IDLE";
-            }
+                Debug.WriteLine("PRESSED " + e.KeyPressed.ToString());
 
-            if (e.KeyPressed == Key.F6 && MidiFilesList.Items.Count > 0 && (playback == null || !playback.IsRunning))
-            {
+                if (e.KeyPressed == (Key)Enum.Parse(typeof(Key), (string)stopKeyButton.Content) && playback != null && playback.IsRunning)
                 {
-                    MidiFile midiFile;
-                    try
-                    {
-                        midiFile = MidiFile.Read(midiFiles[MidiFilesList.SelectedIndex]);
-                    }
-                    catch
-                    {
-                        MessageBox.Show("Unable to parse this file as a MIDI file");
-                        return;
-                    }
-
-                    var layout = (KeyboardLayout)((ComboBoxItem)layoutComboBox.SelectedItem).Tag;
-
-                    var player = new MIDIToKeyboardConverter(midiFile, layout, useAutoRoot ? -1 : int.Parse(customRootInput.Text));
-
-                    playback = midiFile.GetPlayback(player);
-                    playback.Start();
-
-                    playingTextBlock.Text = "Playing: " + ((ListBoxItem)MidiFilesList.SelectedItem).Content;
+                    playback.Stop();
+                    playingTextBlock.Text = "IDLE";
                 }
+
+                if (e.KeyPressed == (Key)Enum.Parse(typeof(Key), (string)startKeyButton.Content) && MidiFilesList.Items.Count > 0 && (playback == null || !playback.IsRunning))
+                {
+                    {
+                        MidiFile midiFile;
+                        try
+                        {
+                            midiFile = MidiFile.Read(midiFiles[MidiFilesList.SelectedIndex]);
+                        }
+                        catch
+                        {
+                            MessageBox.Show("Unable to parse this file as a MIDI file");
+                            return;
+                        }
+
+                        var layout = (KeyboardLayout)((ComboBoxItem)layoutComboBox.SelectedItem).Tag;
+
+                        var player = new MIDIToKeyboardConverter(midiFile, layout, useAutoRoot ? -1 : int.Parse(customRootInput.Text));
+
+                        playback = midiFile.GetPlayback(player);
+                        playback.Start();
+
+                        playingTextBlock.Text = "Playing: " + ((ListBoxItem)MidiFilesList.SelectedItem).Content;
+                    }
+                }
+            }
+        }
+
+        private void onStartButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!listeningForStartKey)
+            {
+                startKeyBackup = (string)startKeyButton.Content;
+                startKeyButton.Content = "press a key";
+            }
+            else
+            {
+                startKeyButton.Content = startKeyBackup;
+            }
+            listeningForStartKey = !listeningForStartKey;
+        }
+
+        private void onStartButtonKeyDown(object sender, KeyEventArgs e)
+        {
+            if (listeningForStartKey)
+            {
+                startKeyButton.Content = e.Key.ToString();
+                listeningForStartKey = false;
+                save();
+            }
+        }
+
+        private void onStopButtonClick(object sender, RoutedEventArgs e)
+        {
+            if (!listeningForStopKey)
+            {
+                stopKeyBackup = (string)stopKeyButton.Content;
+                stopKeyButton.Content = "press a key";
+            }
+            else
+            {
+                stopKeyButton.Content = stopKeyBackup;
+            }
+            listeningForStopKey = !listeningForStopKey;
+        }
+
+        private void onStopButtonKeyDown(object sender, KeyEventArgs e)
+        {
+            if (listeningForStopKey)
+            {
+                stopKeyButton.Content = e.Key.ToString();
+                listeningForStopKey = false;
+                save();
             }
         }
 
@@ -161,6 +216,8 @@ namespace GenshinLyrePlayer
             {
                 var config = new Config();
                 config.keyboardLayout = (string)((ComboBoxItem)layoutComboBox.SelectedItem).Content;
+                config.startKey = (string)startKeyButton.Content;
+                config.stopKey = (string)stopKeyButton.Content;
                 config.useAutoRoot = (bool)autoRootCheckbox.IsChecked;
                 if (!config.useAutoRoot)
                 {
@@ -177,13 +234,40 @@ namespace GenshinLyrePlayer
 
         private void loadSave()
         {
-            var config = JsonSerializer.Deserialize<Config>(File.ReadAllText("config.json"));
-            layoutComboBox.Text = config.keyboardLayout; // this doesn't just change the text value but will select the right ComboBoxItem matching
-            autoRootCheckbox.IsChecked = config.useAutoRoot;
-            if (!config.useAutoRoot)
+            try
             {
-                customRootInput.Text = config.customRoot.ToString();
+                var config = JsonSerializer.Deserialize<Config>(File.ReadAllText("config.json"));
+
+                autoRootCheckbox.IsChecked = config.useAutoRoot;
+                
+                if (!config.useAutoRoot)
+                {
+                    customRootInput.Text = config.customRoot.ToString();
+                }
+
+                // check if the keyboard layout is valid by trying to convert it to its KeyboardLayout variant
+                try
+                {
+                    Enum.Parse(typeof(KeyboardLayout), config.keyboardLayout);
+                    layoutComboBox.Text = config.keyboardLayout; // this doesn't just change the text value but will select the right ComboBoxItem matching
+                }
+                catch {}
+
+                // check if the keys in the config are valid by trying to convert them to their Key variant
+                try
+                {
+                    Enum.Parse(typeof(Key), config.startKey);
+                    startKeyButton.Content = config.startKey;
+                } catch {}
+
+                try
+                {
+                    Enum.Parse(typeof(Key), config.stopKey);
+                    stopKeyButton.Content = config.stopKey;
+                } catch {}
             }
+            catch {}
+
             configLoaded = true;
         }
     }
